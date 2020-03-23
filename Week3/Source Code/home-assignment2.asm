6697ecf1fbe1ff325c8ac5e099f614f3c96c575d
Thanhhff <thanhnguyen200399@gmail.com>
2020-03-23 19:02:09 +0700

    Update home-assignment2.asm

--- Week3/Source Code/home-assignment2.asm
+++ Week3/Source Code/home-assignment2.asm
@@ -1,16 +1,19 @@
-#Laboratory Exercise 3, Home Assignment 1
+.data 
+	arr: .word 0,0,10,0,1,1
 
-.text 
-	addi $s1, $zero, 2 # i = 2
-	addi $s2, $zero, 1 # j = 1
+.text
+	li $s1, -1
+	la $s2, arr
+	li $s3, 5
+	li $s4, 1
+	li $s5, 0	
 
-start:
-	slt $t0, $s2, $s1         # j < i
-	bne $t0, $zero, else 	  # branch to else if j<i
-	addi $t1, $t1, 1	  # then part: x=x+1
-	addi $t3, $zero, 1        # z=1
-	j endif   		  # skip “else” part
-else:
-	addi $t2,$t2,-1   # begin else part: y=y-1
-	add $t3,$t3,$t3 # z=2*z 
-endif:
+loop:
+	add $s1,$s1,$s4  #i=i+step
+	add $t1,$s1,$s1  #t1=2*s1
+	add $t1,$t1,$t1  #t1=4*s1
+	add $t1,$t1,$s2  #t1 store the address of A[i]
+	lw  $t0,0($t1)    #load value of A[i] in $t0
+	add $s5,$s5,$t0  #sum=sum+A[i]
+	
+	beq $zero, $t0, loop
